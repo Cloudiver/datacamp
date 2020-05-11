c37a41e9318c1a0453a400f892078606efb169a0
zzibuyu <husterfan@qq.com>
2020-05-11 22:28:38 +0800

    新增04Data Manipulation With Pandas

---
+++ 04Data Manipulation With Pandas/DataFrame基本属性和方法.py
@@ -0,0 +1,11 @@
+# Print the head of the homelessness data
+print(homelessness.head())  # 输出前五行数据
+
+# Print information about homelessness
+print(homelessness.info())  # 表格的基本信息
+
+# Print the shape of homelessness
+print(homelessness.shape)   # (x, y) 表格的行数和列数
+
+# Print a description of homelessness
+print(homelessness.describe())  # 快速计算float , int 列的基本统计信息
